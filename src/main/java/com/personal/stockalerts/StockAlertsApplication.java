3ba1bf44a49bd13d2396018e05bd37b22b90dbe9
johnny-hughes <johnny.hughes@autotrader.co.uk>
2020-11-23 14:06:42 +0000

    Config files

---
+++ src/main/java/com/personal/stockalerts/StockAlertsApplication.java
@@ -0,0 +1,13 @@
+package com.personal.stockalerts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockAlertsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockAlertsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/personal/stockalerts/StockAlertsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.personal.stockalerts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockAlertsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
